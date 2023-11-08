15edcc5d61b6dbe21d00969fb076538c0397f5ad
Germán David <manchov12@gmail.com>
2023-11-08 10:27:28 -0500

    creando componente Layout y Card

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const activeStyle = "underline underline-offset-4"
 
   return (
-    <nav className='flex justify-between items-center w-full py-5 px-8 text-sm font-light fixed z-10' >
+    <nav className='flex justify-between items-center w-full py-5 px-8 text-sm font-light fixed z-10 top-0' >
         <ul className='flex items-center gap-3' >
             <li className='font-semibold text-lg'>
                 <NavLink
