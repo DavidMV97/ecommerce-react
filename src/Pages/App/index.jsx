0c1fbafebb07a6aef03819811fec9e64819e0571
Germán David <manchov12@gmail.com>
2023-11-11 18:03:40 -0500

    Creando constexto global carritoi

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -4,30 +4,35 @@ import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
-import SignIn from '../SignIn' 
+import SignIn from '../SignIn'
 import './App.css'
 import Navbar from '../../Components/Navbar'
+import { ShoppingCartProvider } from '../../Context'
+
+
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    {path: '/', element: <Home />},
-    {path: '/my-account', element: <MyAccount />},
-    {path: '/my-order', element: <MyOrder />},
-    {path: '/my-orders', element: <MyOrders />},
-    {path: '/sign-in', element: <SignIn />},
-    {path: '/*', element: <NotFound />}
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }
   ])
 
-  return routes; 
+  return routes;
 }
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <AppRoutes />
-      <Navbar/>
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes />
+        <Navbar />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
